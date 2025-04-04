4cca73a356538c8f09e6d99cfa0a279b93fbd065
Shivam Tiwari <72348073+shivam16122000@users.noreply.github.com>
2025-04-04 12:34:34 +0530

    Create sparkAdhoc.scala for running analytical query on delta lake

---
+++ sparkAdhoc.scala
@@ -0,0 +1,27 @@
+/opt/homebrew/Cellar/apache-spark/3.5.1/bin/spark-shell \
+  --packages io.delta:delta-spark_2.12:3.1.0,org.apache.spark:spark-sql-kafka-0-10_2.12:3.5.1 \
+  --conf spark.sql.extensions=io.delta.sql.DeltaSparkSessionExtension \
+  --conf spark.sql.catalog.spark_catalog=org.apache.spark.sql.delta.catalog.DeltaCatalog
+
+import org.apache.spark.sql.SparkSession
+import org.apache.spark.sql.functions._
+import org.apache.spark.sql.types._
+import io.delta.tables._
+
+val spark = SparkSession.builder
+  .appName("DomainEventIngestion")
+  .config("spark.sql.extensions", "io.delta.sql.DeltaSparkSessionExtension")
+  .config("spark.sql.catalog.spark_catalog", "org.apache.spark.sql.delta.catalog.DeltaCatalog")
+  .getOrCreate()
+
+spark.sparkContext.setLogLevel("WARN")
+
+val deltaTablePath = "hdfs://localhost:9001/delta/order_events"
+
+spark.sql(s"CREATE TABLE order_events USING DELTA LOCATION '$deltaTablePath'")
+
+val resultDF = spark.sql("SELECT * FROM order_events")
+resultDF.show()
+
+ spark.sql("SELECT * FROM order_events version as of 1").show(false)
+ spark.sql("SELECT * FROM order_events").show(false)
